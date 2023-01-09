f9673db7d7f60d5bf749094fea2a74884fbf08d8
Carol Moreira <caahrolmoreira@gmail.com>
2023-01-09 19:11:12 -0300

    dom-I-exercicios

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-const frutas = ["laranja", "limão", "uva"];
+//exercicio 1
+const frutas = ["laranja", "limão", "uva"]
+
+document.getElementById("fruta-1").innerHTML = frutas[0]
+document.getElementById("fruta-2").innerHTML = frutas[1]
+document.getElementById("fruta-3").innerHTML = frutas[2]
+
+
+// const fruta1 = document.getElementById("fruta-1")
+// fruta1.innerHTML = frutas[0]
+
+// const fruta2 = document.getElementById("fruta-2")
+// fruta2.innerHTML = frutas[1]
+
+// const fruta3 = document.getElementById("fruta-3")
+// fruta3.innerHTML = frutas[2]
+
+
+//looping
+// for (let i = 0; i < frutas.length; i++) {
+//   document.getElementById(`fruta-${i+1}`).innerHTML = frutas[i]
+  
+// }
+
+
+//exercicio2
+const inputElement = document.getElementById("nova-fruta")
+
+function novaFrutaChange () {
+  console.log(inputElement.value)
+}
+
+inputElement.addEventListener("input", novaFrutaChange);
+//toda vez que acontecer o evento de input, a função novaFrutaChange é chamada
+
+//exercicio3
+
+function adicionaFruta () {
+  document.getElementById("fruta-4").innerHTML =
+  document.getElementById("nova-fruta").value
+  frutas.push(document.getElementById("nova-fruta").value)
+}
